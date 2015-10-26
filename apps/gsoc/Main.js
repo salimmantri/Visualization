68592ae4e9d9fba496c1e05fd22e0087128e6c3d
Gordon Smith <gordon.smith@lexisnexis.com>
2015-10-26 09:17:44 +0000

    WIP Add impress
    Signed-off-by: Gordon Smith <gordon.smith@lexisnexis.com>

---
+++ apps/gsoc/Main.js
@@ -0,0 +1,14 @@
+﻿"use strict";
+(function (root, factory) {
+    if (typeof define === "function" && define.amd) {
+        define(["src/layout/Border"], factory);
+    }
+}(this, function (Border) {
+    function Main(target) {
+        Border.call(this);
+    }
+    Main.prototype = Object.create(Border.prototype);
+    Main.prototype.constructor = Main;
+
+    return Main;
+}));
