48187c9d80e41fbb2250acbb786449cfd4625b24
Gordon Smith <gordon.smith@lexisnexis.com>
2015-11-01 10:51:17 +0000

    WIP - Outline
    Signed-off-by: Gordon Smith <gordon.smith@lexisnexis.com>

--- apps/gsoc/Main.js
+++ apps/gsoc/Main.js
@@ -1,14 +1,36 @@
 ﻿"use strict";
 (function (root, factory) {
     if (typeof define === "function" && define.amd) {
-        define(["src/layout/Border"], factory);
+        define(["src/layout/Grid", "src/chart/Summary", "src/form/Slider", "src/chart/Column"], factory);
     }
-}(this, function (Border) {
+}(this, function (Grid, Summary, Slider, Column) {
     function Main(target) {
-        Border.call(this);
+        Grid.call(this);
     }
-    Main.prototype = Object.create(Border.prototype);
+    Main.prototype = Object.create(Grid.prototype);
     Main.prototype.constructor = Main;
+
+    Main.prototype.enter = function (domNode, element) {
+        Grid.prototype.enter.apply(this, arguments);
+        this.slider = new Slider()
+            .showPlay(true)
+        ;
+        this.setContent(0, 0, this.slider, null, 2, 6);
+        this.summary = new Summary()
+            .fixedSize(false)
+            .columns(["Test"])
+            .data([["Calue"]])
+        ;
+        this.setContent(0, 6, this.summary, null, 2, 3);
+        this.column = new Column();
+        this.setContent(2, 0, this.column, null, 6, 9);
+    }
+    Main.prototype.update = function (domNode, element) {
+        Grid.prototype.update.apply(this, arguments);
+    }
+    Main.prototype.exit = function (domNode, element) {
+        Grid.prototype.exit.apply(this, arguments);
+    }
 
     return Main;
 }));
