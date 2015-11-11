ea8f2b79eb79dbfb7ab4fdcd3a49346674e9ddbc
Gordon Smith <gordon.smith@lexisnexis.com>
2015-11-11 02:10:26 +0000

    Move platform/polyfills into separate file.
    Signed-off-by: Gordon Smith <gordon.smith@lexisnexis.com>

--- src/common/Widget.js
+++ src/common/Widget.js
@@ -1,29 +1,15 @@
 "use strict";
 (function (root, factory) {
     if (typeof define === "function" && define.amd) {
-        define(["d3", "./Class", "./PropertyExt", "./Database"], factory);
+        define(["d3", "./Class", "./Platform", "./PropertyExt", "./Database"], factory);
     } else {
-        root.require = root.require || function (paths, cb) {
-            if (typeof paths === "function") {
-                cb = paths;
-                paths = [];
-            }
-
-            var objs = paths.map(function (path) {
-                var pathIdx = path.indexOf("src/") === 0 ? "src/".length : 0;
-                var prop = path.substring(pathIdx).split("/").join("_");
-                return root[prop];
-            });
-
-            cb.apply(null, objs);
-        };
-
-        root.common_Widget = factory(root.d3, root.common_Class, root.common_PropertyExt, root.common_Database);
+        root.common_Widget = factory(root.d3, root.common_Class, root.common_Platform, root.common_PropertyExt, root.common_Database);
     }
-}(this, function (d3, Class, PropertyExt, Database) {
+}(this, function (d3, Class, Platform, PropertyExt, Database) {
     var widgetID = 0;
     function Widget() {
         Class.call(this);
+        Platform.call(this);
         PropertyExt.call(this);
         this._class = Object.getPrototypeOf(this)._class;
         this._id = "_w" + widgetID++;
@@ -58,6 +44,7 @@
     Widget.prototype = Object.create(Class.prototype);
     Widget.prototype.constructor = Widget;
     Widget.prototype._class = "common_Widget";
+    Widget.prototype.mixin(Platform);
     Widget.prototype.mixin(PropertyExt);
 
     Widget.prototype.leakCheck = function (newNode) {
@@ -101,91 +88,6 @@
             }
         }
     };
-
-    Widget.prototype.ieVersion = (function () {
-        var ua = navigator.userAgent, tem,
-            M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
-        if (/trident/i.test(M[1])) {
-            tem = /\brv[ :]+(\d+)/g.exec(ua) || [];
-            return parseFloat(tem[1]);
-        }
-        if (/msie/i.test(M[1])) {
-            return parseFloat(M[2]);
-        }
-        return null;
-    })();
-    Widget.prototype.isIE = Widget.prototype.ieVersion !== null;
-    Widget.prototype.svgMarkerGlitch = Widget.prototype.isIE && Widget.prototype.ieVersion <= 12;
-    Widget.prototype.MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver || function (callback) {
-        //  Just enough for HTMLOverlay and C3  ---
-        this.callback = callback;
-        this.listeners = [];
-
-        var MutationListener = function (callback, domNode, type) {
-            this.callback = callback;
-            this.domNode = domNode;
-            this.type = type;
-        };
-        MutationListener.prototype = {
-            handleEvent: function (evt) {
-                var mutation = {
-                    type: this.type,
-                    target: this.domNode,
-                    addedNodes: [],
-                    removedNodes: [],
-                    previousSibling: evt.target.previousSibling,
-                    nextSibling: evt.target.nextSibling,
-                    attributeName: null,
-                    attributeNamespace: null,
-                    oldValue: null
-                };
-                this.callback([mutation]);
-            }
-        };
-
-        this.observe = function (domNode, config) {
-            var listener = null;
-            if (config.attributes) {
-                listener = new MutationListener(this.callback, domNode, "attributes");
-                this.listeners.push(listener);
-                domNode.addEventListener("DOMAttrModified", listener, true);
-            }
-
-            if (config.characterData) {
-                listener = new MutationListener(this.callback, domNode, "characterData");
-                this.listeners.push(listener);
-                domNode.addEventListener("DOMCharacterDataModified", listener, true);
-            }
-
-            if (config.childList) {
-                listener = new MutationListener(this.callback, domNode, "childList");
-                this.listeners.push(listener);
-                domNode.addEventListener("DOMNodeInserted", listener, true);
-                domNode.addEventListener("DOMNodeRemoved", listener, true);
-            }
-        };
-
-        this.disconnect = function () {
-            this.listeners.forEach(function (item) {
-                switch (item.type) {
-                    case "attributes":
-                        item.domNode.removeEventListener("DOMAttrModified", item, true);
-                        break;
-                    case "characterData":
-                        item.domNode.removeEventListener("DOMCharacterDataModified", item, true);
-                        break;
-                    case "childList":
-                        item.domNode.removeEventListener("DOMNodeRemoved", item, true);
-                        item.domNode.removeEventListener("DOMNodeInserted", item, true);
-                        break;
-                }
-            });
-            this.listeners = [];
-        };
-    };
-    if (!window.MutationObserver) {
-        window.MutationObserver = Widget.prototype.MutationObserver;
-    }
 
     //  Events  ---
     Widget.prototype.on = function (eventID, func, stopPropagation) {
@@ -330,32 +232,6 @@
             height: height
         });
         return this;
-    };
-
-    Widget.prototype._scrollBarWidth = null;
-    Widget.prototype.getScrollbarWidth = function () {
-        if (Widget.prototype._scrollBarWidth === null) {
-            var outer = document.createElement("div");
-            outer.style.visibility = "hidden";
-            outer.style.width = "100px";
-            outer.style.msOverflowStyle = "scrollbar";
-
-            document.body.appendChild(outer);
-
-            var widthNoScroll = outer.offsetWidth;
-            outer.style.overflow = "scroll";
-
-            var inner = document.createElement("div");
-            inner.style.width = "100%";
-            outer.appendChild(inner);
-
-            var widthWithScroll = inner.offsetWidth;
-
-            outer.parentNode.removeChild(outer);
-
-            Widget.prototype._scrollBarWidth = widthNoScroll - widthWithScroll;
-        }
-        return Widget.prototype._scrollBarWidth;
     };
 
     Widget.prototype.scale = function (_) {
@@ -607,23 +483,6 @@
     Widget.prototype.update = function (domeNode, element) { };
     Widget.prototype.postUpdate = function (domeNode, element) { };
     Widget.prototype.exit = function (domeNode, element) { };
-
-    //  Util  ---
-    Widget.prototype.debounce = function (func, threshold, execAsap) {
-        return function debounced() {
-            var obj = this || {}, args = arguments;
-            function delayed() {
-                if (!execAsap)
-                    func.apply(obj, args);
-                obj.timeout = null;
-            }
-            if (obj.timeout)
-                clearTimeout(obj.timeout);
-            else if (execAsap)
-                func.apply(obj, args);
-            obj.timeout = setTimeout(delayed, threshold || 100);
-        };
-    };
 
     return Widget;
 }));
