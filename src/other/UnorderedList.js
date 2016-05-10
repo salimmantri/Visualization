6c61b916eb3a0c37e50bfddfe7f4f498324471c0
Gordon Smith <gordonjsmith@gmail.com>
2016-05-10 16:31:11 -0400

    Grrrr
    Signed-off-by: Gordon Smith <gordonjsmith@gmail.com>

--- src/other/UnorderedList.js
+++ src/other/UnorderedList.js
@@ -19,19 +19,46 @@
 
     UnorderedList.prototype.enter = function (domNode, element) {
         HTMLWidget.prototype.enter.apply(this, arguments);
-        this._ul = element.append("ul")
-            .attr("class", "container")
-            .attr("data-depth", 0)
-        ;
     };
 
     UnorderedList.prototype.update = function (domNode, element) {
         HTMLWidget.prototype.update.apply(this, arguments);
-        this.appendArray(this._ul, this.data(), 0);
+        this.updateArray(element, this.data(), 0);
     };
 
-    UnorderedList.prototype.appendArray = function (element, arr, depth) {
-        var inner = element.selectAll('.container[data-depth="' + depth + '"] > .dataRow').data(arr);
+    UnorderedList.prototype.updateArray = function (element, arr, depth) {
+        var data = arr instanceof Array ? [arr] : [];
+        if (depth === 2) {
+            var d = 0;
+        }
+        var innerUL = element.selectAll("ul[data-depth='" + depth + "']").data(data);
+        innerUL.enter().append("ul")
+            .attr("data-depth", depth)
+        ;
+        var context = this;
+        innerUL.each(function (d) {
+            console.log(d === arr);
+            var element = d3.select(this);
+            var inner = element.selectAll('ul[data-depth="' + depth + '"] > .dataRow').data(d);
+            inner.enter().append("li")
+                .attr("class", "dataRow")
+            ;
+            inner
+                .text(function (row) {
+                    return row[0] + " - " + depth;
+                })
+                .each(function (row) {
+                    console.log(row[1]);
+                    context.updateArray(innerUL, row[1], depth + 1);
+                })
+            ;
+            inner.exit().remove();
+        });
+        innerUL.exit().remove()
+    }
+
+    UnorderedList.prototype.updateArrayXXX = function (element, arr, depth) {
+        var inner = element.selectAll('ul[data-depth="' + depth + '"] > .dataRow').data(arr);
         inner.enter().append("li")
             .attr("class", "dataRow")
         ;
@@ -42,13 +69,15 @@
             })
             .each(function (row) {
                 var element = d3.select(this);
-                if (row[1] instanceof Array) {
-                    var innerUl = element.append("ul")
-                        .attr("class", "container")
-                        .attr("data-depth", depth + 1)
-                    ;
-                    context.appendArray(innerUl, row[1], depth + 1);
+                var data = row[1] instanceof Array ? [row[1]] : [];
+                var innerUL = element.selectAll("li > ul[data-depth='" + (depth + 1) + "']").data(data);
+                innerUL.enter().append("ul")
+                    .attr("data-depth", depth + 1)
+                ;
+                if (data.length) {
+                    context.appendArray(innerUL, data[0], depth + 1);
                 }
+                innerUL.exit().remove()
             })
         ;
         inner.exit().remove();
